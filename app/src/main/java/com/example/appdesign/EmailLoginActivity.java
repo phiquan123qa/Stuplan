8ebcd4bd6d2a48949c57369849290ef7719426da
phiquan123qa <phiquan070902@gmail.com>
2023-11-06 20:46:32 +0700

    Second commit

--- app/src/main/java/com/example/appdesign/EmailLoginActivity.java
+++ app/src/main/java/com/example/appdesign/EmailLoginActivity.java
@@ -2,16 +2,58 @@ package com.example.appdesign;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.text.InputType;
+import android.text.method.PasswordTransformationMethod;
+import android.view.MotionEvent;
+import android.view.View;
 import android.view.Window;
+import android.widget.EditText;
+
+import com.example.utility.UtilityKeyboard;
 
 public class EmailLoginActivity extends AppCompatActivity {
 
+    private EditText editTextEmailAddress;
+    private EditText editTextPassword;
+    private boolean isPwVsb = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_email_login);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        View rootLayout = findViewById(R.id.rootLayout);
+        editTextEmailAddress = findViewById(R.id.editTextEmailAddress);
+        editTextPassword = findViewById(R.id.editTextPassword);
 
+
+        rootLayout.setOnTouchListener((view, motionEvent) -> {
+            UtilityKeyboard.hideSoftKeyboard(EmailLoginActivity.this);
+            return false;
+        });
+        editTextEmailAddress.setOnTouchListener((view, event) -> {
+            final int DRAWABLE_RIGHT = 2;
+            if (event.getAction() == MotionEvent.ACTION_UP) {
+                if (event.getRawX() >= (editTextEmailAddress.getRight() - editTextEmailAddress.getCompoundDrawables()[DRAWABLE_RIGHT].getBounds().width())) {
+                    UtilityKeyboard.clearData(editTextEmailAddress);
+                    return true;
+                }
+            }
+            return false;
+        });
+
+        editTextPassword.setOnTouchListener((v, event) -> {
+            final int DRAWABLE_RIGHT = 2;
+            if (event.getAction() == MotionEvent.ACTION_UP) {
+                if (event.getRawX() >= (editTextPassword.getRight() - editTextPassword.getCompoundDrawables()[DRAWABLE_RIGHT].getBounds().width())) {
+                    UtilityKeyboard.togglePasswordVisibility(editTextPassword);
+                    return true;
+                }
+            }
+            return false;
+        });
     }
+
 }
---
+++ app/src/main/java/com/example/utility/UtilityKeyboard.java
@@ -0,0 +1,44 @@
+package com.example.utility;
+
+import android.app.Activity;
+import android.text.InputType;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+
+import com.example.appdesign.R;
+
+public class UtilityKeyboard {
+    public static void hideSoftKeyboard(Activity activity) {
+        InputMethodManager inputMethodManager =
+                (InputMethodManager) activity.getSystemService(
+                        Activity.INPUT_METHOD_SERVICE);
+        if(inputMethodManager.isAcceptingText()){
+            inputMethodManager.hideSoftInputFromWindow(
+                    activity.getCurrentFocus().getWindowToken(),
+                    0
+            );
+        }
+    }
+
+    public static void clearData(EditText editText){
+        editText.getText().clear();
+    }
+
+    public static void togglePasswordVisibility(EditText editTextPassword) {
+        int inputType = editTextPassword.getInputType();
+
+        if (inputType == (InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD)) {
+            // Password is currently visible, so hide it
+            editTextPassword.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+            editTextPassword.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.baseline_visibility_off_24, 0);
+        } else {
+            // Password is currently hidden, so show it
+            editTextPassword.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD);
+            editTextPassword.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.baseline_visibility_24, 0);
+
+        }
+
+        // Move the cursor to the end of the text
+        editTextPassword.setSelection(editTextPassword.getText().length());
+    }
+}

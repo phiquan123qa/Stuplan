e56593aaad160cf0ad22e41a7f8fce5a8ab9e3db
phiquan123qa <phiquan070902@gmail.com>
2023-11-06 15:12:14 +0700

    First commit

---
+++ app/src/main/java/com/example/appdesign/StartActivity.java
@@ -0,0 +1,22 @@
+package com.example.appdesign;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class StartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+        new Handler().postDelayed(() -> {
+            Intent intent = new Intent(StartActivity.this, LoginActivity.class);
+            startActivity(intent);
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+            finish();
+        }, 3000);
+    }
+}

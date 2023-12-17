67bf24a6eedc80b6a6da9fad001e29935b358aea
phiquan123qa <phiquan070902@gmail.com>
2023-12-17 16:19:35 +0700

    Finish create new issue, project need fix show icon

--- app/src/main/java/com/vn/appdesign/IssueFragment.java
+++ app/src/main/java/com/vn/appdesign/IssueFragment.java
@@ -1,24 +1,32 @@
 package com.vn.appdesign;
 
+import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toolbar;
 
+import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.vn.models.Issue;
+import com.vn.models.IssueStatusEnum;
 import com.vn.models.Project;
 import com.vn.utility.AdapterIssuesList;
 import com.vn.utility.AdapterProjectsList;
@@ -47,6 +55,9 @@ public class IssueFragment extends Fragment {
     DatabaseReference reference;
     AdapterIssuesList adapterIssuesList;
     List<Issue> listIssues;
+    ColorStateList def;
+    TextView item1, item2, item3, select;
+    Integer selectedTab = 0;
 
     public IssueFragment() {
         // Required empty public constructor
@@ -81,15 +92,65 @@ public class IssueFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         View view = inflater.inflate(R.layout.fragment_issue, container, false);
+        Button buttonBack = view.findViewById(R.id.btn_back_to_project) ;
+        TextView titleTextView = view.findViewById(R.id.title_issue_fragment);
+        item1 = view.findViewById(R.id.item_select1);
+        item2 = view.findViewById(R.id.item_select2);
+        item3 = view.findViewById(R.id.item_select3);
+        select = view.findViewById(R.id.selected_tab);
+        def = item2.getTextColors();
         Bundle arguments = getArguments();
         String projectId = null;
         if (arguments != null) {
             projectId = arguments.getString("projectId");
             if (projectId != null) {
                 //projectID = arguments.getString(PRJ_ID);
+                fetchProjectName(projectId, titleTextView);
             }
         }
+        String finalProjectId = projectId;
+        item1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                select.animate().x(0).setDuration(100);
+                item1.setTextColor(Color.WHITE);
+                item2.setTextColor(def);
+                item3.setTextColor(def);
+                selectedTab = 0;
+                updateListBasedOnSelectedTab(selectedTab, listIssues, reference, finalProjectId, view);
+                adapterIssuesList.notifyDataSetChanged();
+
+            }
+        });
+        item2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int size = item2.getWidth();
+                select.animate().x(size).setDuration(100);
+                item1.setTextColor(def);
+                item2.setTextColor(Color.WHITE);
+                item3.setTextColor(def);
+                selectedTab = 1;
+                updateListBasedOnSelectedTab(selectedTab, listIssues, reference, finalProjectId, view);
+                adapterIssuesList.notifyDataSetChanged();
+            }
+        });
+        item3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int size = item3.getWidth()*2;
+                select.animate().x(size).setDuration(100);
+                item1.setTextColor(def);
+                item2.setTextColor(def);
+                item3.setTextColor(Color.WHITE);
+                selectedTab = 2;
+                updateListBasedOnSelectedTab(selectedTab, listIssues, reference, finalProjectId, view);
+                adapterIssuesList.notifyDataSetChanged();
+            }
+        });
+
         recyclerView = view.findViewById(R.id.recycler_view_list_issue);
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
@@ -97,33 +158,56 @@ public class IssueFragment extends Fragment {
         adapterIssuesList = new AdapterIssuesList(getContext(), listIssues);
         recyclerView.setAdapter(adapterIssuesList);
         reference = FirebaseDatabase.getInstance().getReference("ISSUE");
-        String finalProjectId = projectId;
-        reference.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                List<Issue> newList = new ArrayList<>();
-                String projectIdd = finalProjectId;
-                for(DataSnapshot dataSnapshot : snapshot.getChildren()){
-                    Issue issue = dataSnapshot.getValue(Issue.class);
-                    if (issue != null && projectIdd.equals(issue.getIdProject())) {
-                        newList.add(issue);
-                    }
-                }
-                listIssues.clear();
-                listIssues.addAll(newList);
-                adapterIssuesList.notifyDataSetChanged();
-                if (listIssues.isEmpty()) {
-                    // If empty, show a TextView or handle it in your UI
-                    showEmptyIssuesNotification(view, 1);
-                } else {
-                    // If not empty, hide the notification
-                    showEmptyIssuesNotification(view, 0);
-                }
-            }
+        updateListBasedOnSelectedTab(selectedTab, listIssues, reference, finalProjectId, view);
+//        reference.addValueEventListener(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(@NonNull DataSnapshot snapshot) {
+//                List<Issue> newList = new ArrayList<>();
+//                String projectIdd = finalProjectId;
+//                for(DataSnapshot dataSnapshot : snapshot.getChildren()){
+//                    Issue issue = dataSnapshot.getValue(Issue.class);
+//                    if (issue != null && projectIdd.equals(issue.getIdProject())) {
+//                        switch (selectedTab) {
+//                            case 0: // TODO tab
+//                                if (IssueStatusEnum.TODO.equals(issue.getStatus())) {
+//                                    newList.add(issue);
+//                                }
+//                                break;
+//                            case 1: // DOING tab
+//                                if (IssueStatusEnum.DOING.equals(issue.getStatus())) {
+//                                    newList.add(issue);
+//                                }
+//                                break;
+//                            case 2: // DONE tab
+//                                if (IssueStatusEnum.DONE.equals(issue.getStatus())) {
+//                                    newList.add(issue);
+//                                }
+//                                break;
+//                        }
+//                    }
+//                }
+//                listIssues.clear();
+//                listIssues.addAll(newList);
+//                adapterIssuesList.notifyDataSetChanged();
+//                if (listIssues.isEmpty()) {
+//                    // If empty, show a TextView or handle it in your UI
+//                    showEmptyIssuesNotification(view, 1);
+//                } else {
+//                    // If not empty, hide the notification
+//                    showEmptyIssuesNotification(view, 0);
+//                }
+//            }
+//
+//            @Override
+//            public void onCancelled(@NonNull DatabaseError error) {
+//
+//            }
+//        });
 
+        buttonBack.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-
+            public void onClick(View v) {
+                navigateBackToProjectFragment();
             }
         });
 
@@ -139,6 +223,85 @@ public class IssueFragment extends Fragment {
             RelativeLayout emptyIssuesNotification = view.findViewById(R.id.container_link_add_new_issue);
             emptyIssuesNotification.setVisibility(View.GONE);
         }
+
+    }
+    private void navigateBackToProjectFragment() {
+        // Get the FragmentManager
+        FragmentManager fragmentManager = requireActivity().getSupportFragmentManager();
+
+        // Pop the back stack to go back to the ProjectFragment
+        fragmentManager.popBackStack();
+    }
+    private void fetchProjectName(String projectId, TextView titleTextView) {
+        DatabaseReference projectReference = FirebaseDatabase.getInstance().getReference("PROJECT").child(projectId);
+        projectReference.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (snapshot.exists()) {
+                    Project project = snapshot.getValue(Project.class);
+                    if (project != null) {
+                        String projectName = project.getTitle();
+                        int maxLength = 8;
+                        String truncatedTitle = projectName.length() > maxLength ? projectName.substring(0, maxLength) + "..." : projectName;
+                        titleTextView.append(": " + truncatedTitle);
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                // Handle error if needed
+            }
+        });
+    }
+    private void updateListBasedOnSelectedTab(int selectedTab, List<Issue> listIssues, DatabaseReference reference,String finalProjectId, View view) {
+        // You can implement additional logic here if needed
+        // For example, update the UI or perform other actions based on the selected tab
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                List<Issue> newList = new ArrayList<>();
+                String projectIdd = finalProjectId;
+                for(DataSnapshot dataSnapshot : snapshot.getChildren()){
+                    Issue issue = dataSnapshot.getValue(Issue.class);
+                    if (issue != null && projectIdd.equals(issue.getIdProject())) {
+                        switch (selectedTab) {
+                            case 0: // TODO tab
+                                if (IssueStatusEnum.TODO.equals(issue.getStatus())) {
+                                    newList.add(issue);
+                                }
+                                break;
+                            case 1: // DOING tab
+                                if (IssueStatusEnum.DOING.equals(issue.getStatus())) {
+                                    newList.add(issue);
+                                }
+                                break;
+                            case 2: // DONE tab
+                                if (IssueStatusEnum.DONE.equals(issue.getStatus())) {
+                                    newList.add(issue);
+                                }
+                                break;
+                        }
+                    }
+                }
+                listIssues.clear();
+                listIssues.addAll(newList);
+                adapterIssuesList.notifyDataSetChanged();
+                if (listIssues.isEmpty()&&selectedTab==0) {
+                    // If empty, show a TextView or handle it in your UI
+                    showEmptyIssuesNotification(view, 1);
+                } else {
+                    // If not empty, hide the notification
+                    showEmptyIssuesNotification(view, 0);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
 
     }
 }
--- app/src/main/java/com/vn/utility/AdapterIssuesList.java
+++ app/src/main/java/com/vn/utility/AdapterIssuesList.java
@@ -1,6 +1,7 @@
 package com.vn.utility;
 
 import android.content.Context;
+import android.text.format.DateUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,6 +20,7 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 import java.util.Locale;
+import java.util.concurrent.TimeUnit;
 import java.util.logging.SimpleFormatter;
 
 public class AdapterIssuesList extends RecyclerView.Adapter<AdapterIssuesList.AdapterIssuesListHolder> {
@@ -47,11 +49,30 @@ public class AdapterIssuesList extends RecyclerView.Adapter<AdapterIssuesList.Ad
         }
         Date createDate = issue.getDateCreate();
 
-        SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy HH:mm", Locale.getDefault());
-        String formattedDate = dateFormat.format(createDate);
+//        SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy HH:mm", Locale.getDefault());
+//        String formattedDate = dateFormat.format(createDate);
+//
+//        holder.createTimeIssue.setText(formattedDate);
+        long timeDifference = System.currentTimeMillis() - createDate.getTime();
 
+        // Convert milliseconds to days
+        long daysDifference = TimeUnit.MILLISECONDS.toDays(timeDifference);
+
+        String formattedDate;
+        if (daysDifference == 0) {
+            // Less than 1 day, show time elapsed in hours and minutes
+            formattedDate = DateUtils.getRelativeTimeSpanString(createDate.getTime(), System.currentTimeMillis(), DateUtils.HOUR_IN_MILLIS).toString();
+        } else if (daysDifference == 1) {
+            // 1 day ago
+            formattedDate = "Yesterday " + new SimpleDateFormat("HH:mm", Locale.getDefault()).format(createDate);
+        } else if (daysDifference <= 7) {
+            // 2 to 7 days ago
+            formattedDate = daysDifference + " days ago " + new SimpleDateFormat("HH:mm", Locale.getDefault()).format(createDate);
+        } else {
+            // More than 7 days ago, show the full date and time
+            formattedDate = new SimpleDateFormat("dd-MM-yyyy HH:mm", Locale.getDefault()).format(createDate);
+        }
         holder.createTimeIssue.setText(formattedDate);
-
     }
     private int getIconResourceId(Context context, String iconName) {
         if(iconName != null && !iconName.isEmpty()) {
